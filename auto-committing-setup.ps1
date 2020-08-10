50ca412189b480ee9620dc69a994afa1986e2009
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-08-10 13:50:59 -0400

    Update auto-committing-setup.ps1

--- auto-committing-setup.ps1
+++ auto-committing-setup.ps1
@@ -4,7 +4,7 @@ $PracticeRepoDir='C:\Users\K325\desktop\powershell-practice'
 $CommitMessage='auto committed from auto-committing-setup.ps1!'
 
 # fork and clone this repo into the powershell-studio directory
-# git clone "https://github.com/$GitHubUsername/powershell-practice" "$PracticeRepoDir"
+git clone "https://github.com/$GitHubUsername/powershell-practice" "$PracticeRepoDir"
 
 Copy-Item C:\Users\K325\desktop\auto-committing-setup.ps1 C:\Users\K325\desktop\powershell-practice\auto-committing-setup.ps1
 
